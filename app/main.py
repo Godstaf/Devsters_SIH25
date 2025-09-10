4bb4b52d89dac109077ad8aa0ad796385bea4a6e
Godstar <134953521+Godstaf@users.noreply.github.com>
2025-09-10 17:39:47 +0530

    Enhance FastAPI application by adding Jinja2 templating support and static file serving; update root endpoint to render an HTML template instead of a JSON response. Also, include Jinja2 in requirements.txt.

--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from app.routers import items
 
 app = FastAPI()
-
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(items.router)
 
-@app.get("/")
-def read_root():
-    return {"message": "Welcome to the FastAPI project!"}
+@app.get("/", response_class=HTMLResponse)
+def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
